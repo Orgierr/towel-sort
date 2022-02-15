706cbf5e9b4c026e0e0b3358057ae3a6f1d33434
Igor.K <infinitemind625@gmail.com>
2022-02-15 22:04:23 +0200

    feat:add sort

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-
 // You should implement your task here.
-
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    let sorted = [];
+    if (!matrix) return sorted;
+    for (let index = 0; index < matrix.length; index++) {
+        if ((index + 1) % 2 === 0) {
+            matrix[index].reverse();
+        }
+        sorted = [...sorted, ...matrix[index]];
+    }
+    return sorted;
+};
